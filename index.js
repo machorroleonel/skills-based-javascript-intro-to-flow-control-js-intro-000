8884b851aae00c975cdec09672c87850b5ffbd1d
leonelm <leonelm37710@students.laalliance.org>
2016-11-02 16:41:05 +0000

    Save my password.

---
+++ index.js
@@ -0,0 +1,21 @@
+// write your code below!
+
+function basicTeenager(age) {
+ if (age >= 13 && age <= 19);
+}
+
+function teenager(age) {
+
+}
+
+function ageChecker(age) {
+
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
